583f9bd17189589985450c4cec6bc3a472ca0d3f
theoliverlear <sigwartholiver2@gmail.com>
2023-11-23 14:38:59 -0600

    Add file
    Took 3 minutes

---
+++ src/main/java/org/theoliverlear/v2/MutedBoard.java
@@ -0,0 +1,42 @@
+package org.theoliverlear.v2;
+
+import java.util.ArrayList;
+
+public class MutedBoard extends Board {
+
+    public MutedBoard() {
+        super();
+    }
+    ArrayList<BoardIndex> mutedBoardIndices = new ArrayList<>();
+    public MutedBoard(int[][] mutedBoard) {
+        super(mutedBoard);
+        this.fetchMutedIndices();
+    }
+    public MutedBoard(Board board) {
+        super(board.getBoard());
+        this.fetchMutedIndices();
+    }
+    public void fetchMutedIndices() {
+        for (int row = 0; row < this.rowLength; row++) {
+            for (int column = 0; column < this.colLength; column++) {
+                if (this.board[row][column] != 0) {
+                    this.muteBoardIndex(row + 1, column + 1);
+                }
+            }
+        }
+    }
+    //=============================-Overrides-================================
+    @Override
+    public void placeNumber(int row, int column, int number) {
+        int[] potentialPlaceIndex = {row - 1, column - 1};
+        BoardIndex boardIndex = new BoardIndex(potentialPlaceIndex);
+        if (!this.mutedBoardIndices.contains(boardIndex)) {
+            super.placeNumber(row, column, number);
+        }
+    }
+    public void muteBoardIndex(int row, int column) {
+        int[] muteIndex = {row - 1, column - 1};
+        BoardIndex boardIndex = new BoardIndex(muteIndex);
+        this.mutedBoardIndices.add(boardIndex);
+    }
+}
