db5b73082ffe3e82b5c43429b54c89d5ad3cf880
theoliverlear <sigwartholiver2@gmail.com>
2023-11-23 14:32:57 -0600

    Add equals and hashCode methods
    Took 2 minutes

--- src/main/java/org/theoliverlear/v2/BoardIndex.java
+++ src/main/java/org/theoliverlear/v2/BoardIndex.java
@@ -1,5 +1,7 @@
 package org.theoliverlear.v2;
 
+import java.util.Arrays;
+
 public class BoardIndex {
     int rowIndex;
     int columnIndex;
@@ -7,17 +9,34 @@ public class BoardIndex {
         this.rowIndex = rowIndex;
         this.columnIndex = columnIndex;
     }
+    public BoardIndex(int[] index) {
+        this.rowIndex = index[0];
+        this.columnIndex = index[1];
+    }
     public int getRowIndex() {
         return this.rowIndex;
     }
     public int getColumnIndex() {
         return this.columnIndex;
     }
-    public boolean equals(BoardIndex other) {
-        return this.rowIndex == other.getRowIndex() && this.columnIndex == other.getColumnIndex();
-    }
+    //=============================-Overrides-================================
+    @Override
     public String toString() {
         return "Row: " + this.rowIndex + ", Column: " + this.columnIndex;
     }
-
+    @Override
+    public boolean equals(Object obj) {
+        // If the object is null or not an instance of Board, then it is not
+        // equal to this board.
+        if (obj == null || !(obj instanceof BoardIndex)) {
+            return false;
+        }
+        BoardIndex board = (BoardIndex) obj;
+        return this.rowIndex == board.getRowIndex() &&
+                this.columnIndex == board.getColumnIndex();
+    }
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(new int[] {this.rowIndex, this.columnIndex});
+    }
 }
