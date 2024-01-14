458102c3f604d43be470ef2ba5459259c1889dd1
theoliverlear <sigwartholiver2@gmail.com>
2024-01-14 13:30:36 -0600

    Add header comments
    Took 1 minute

--- src/main/java/org/theoliverlear/service/UserService.java
+++ src/main/java/org/theoliverlear/service/UserService.java
@@ -3,28 +3,36 @@ package org.theoliverlear.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.theoliverlear.model.user.login.LoginRequest;
+import org.theoliverlear.comm.login.LoginRequest;
 import org.theoliverlear.repository.UserRepository;
 
 @Service
 public class UserService {
+    //=============================-Variables-================================
     private UserRepository userRepository;
     private BCryptPasswordEncoder passwordEncoder;
 
+    //============================-Constructors-==============================
     @Autowired
     public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
+    //=============================-Methods-==================================
+
+    //---------------------------Contains-User--------------------------------
     public boolean repositoryContainsUser(LoginRequest loginRequest) {
         return false;
     }
+    //---------------------------Password-Matches-----------------------------
     public boolean passwordMatches(LoginRequest loginRequest) {
         return false;
     }
+    //-------------------------------Login------------------------------------
     public boolean login(LoginRequest loginRequest) {
         return false;
     }
+    //-------------------------------Signup-----------------------------------
     public boolean signup(LoginRequest loginRequest) {
         return false;
     }
@@ -42,5 +50,4 @@ public class UserService {
     public void setPasswordEncoder(BCryptPasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
-
 }
