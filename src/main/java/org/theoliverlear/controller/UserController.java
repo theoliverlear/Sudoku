03ca8bd76233365c66f224a5f7850a8fd882880a
theoliverlear <sigwartholiver2@gmail.com>
2024-01-14 13:29:20 -0600

    Refactor URL path to generalize controller functionality
    Took 39 seconds

--- src/main/java/org/theoliverlear/controller/UserController.java
+++ src/main/java/org/theoliverlear/controller/UserController.java
@@ -1,16 +1,21 @@
 package org.theoliverlear.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.theoliverlear.model.user.login.LoginRequest;
+import org.theoliverlear.comm.login.LoginRequest;
+import org.theoliverlear.service.UserService;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/user")
 public class UserController {
-    @RequestMapping("/")
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/login")
     public ResponseEntity<String> login(@PathVariable LoginRequest loginRequest) {
         return new ResponseEntity<>("true", HttpStatus.OK);
     }
