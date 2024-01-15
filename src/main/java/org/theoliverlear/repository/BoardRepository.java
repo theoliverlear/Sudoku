23937e764c5d6196f4b6df07cbe38388f99236dc
theoliverlear <sigwartholiver2@gmail.com>
2024-01-15 17:35:39 -0600

    Refactor to integrate JPA implementations
    Took 1 minute

--- src/main/java/org/theoliverlear/repository/BoardRepository.java
+++ src/main/java/org/theoliverlear/repository/BoardRepository.java
@@ -1,32 +1,9 @@
 package org.theoliverlear.repository;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.Board;
+import org.theoliverlear.entity.User;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-@Repository
-public class BoardRepository extends SudokuRepository {
-    //===========================-Constructors-===============================
-    public BoardRepository() {
-        super();
-    }
-    //=============================-Methods-==================================
-
-    //---------------------------Contains-Board-------------------------------
-    public boolean containsBoard(Long id) {
-        String sql = "SELECT * FROM boards WHERE id = ?";
-        try (PreparedStatement statement = this.connection.prepareStatement(sql)) {
-            statement.setLong(1, id);
-            return statement.executeQuery().next();
-        } catch (SQLException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-    public boolean containsBoard(Board board) {
-        Long id = board.getId();
-        return this.containsBoard(id);
-    }
-
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Board getBoardById(Long boardId);
 }
