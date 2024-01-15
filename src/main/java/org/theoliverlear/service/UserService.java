cdaf260e2d1707745861e1acc77daf37a238e0a2
theoliverlear <sigwartholiver2@gmail.com>
2024-01-15 17:42:50 -0600

    Begin integrations for JPA to save users and boards to a database
    Took 2 minutes

--- src/main/java/org/theoliverlear/service/UserService.java
+++ src/main/java/org/theoliverlear/service/UserService.java
@@ -3,40 +3,53 @@ package org.theoliverlear.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.theoliverlear.comm.login.LoginRequest;
+import org.theoliverlear.entity.Board;
+import org.theoliverlear.entity.User;
+import org.theoliverlear.repository.BoardRepository;
 import org.theoliverlear.repository.UserRepository;
 
 @Service
 public class UserService {
     //=============================-Variables-================================
     private UserRepository userRepository;
+    private BoardRepository boardRepository;
     private BCryptPasswordEncoder passwordEncoder;
 
     //============================-Constructors-==============================
     @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository,
+                       BoardRepository boardRepository,
+                       BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.boardRepository = boardRepository;
         this.passwordEncoder = passwordEncoder;
     }
     //=============================-Methods-==================================
 
+    //-----------------------------Save-User----------------------------------
+    public void saveUser(User user) {
+        String encodedPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        this.userRepository.save(user);
+    }
+    //-----------------------------Save-Board---------------------------------
+    public void saveBoard(Board board) {
+        this.boardRepository.save(board);
+    }
     //---------------------------Contains-User--------------------------------
-    public boolean repositoryContainsUser(LoginRequest loginRequest) {
-        return false;
+    public boolean containsUser(String username) {
+        return this.userRepository.getUserByUsername(username) != null;
     }
-    //---------------------------Password-Matches-----------------------------
-    public boolean passwordMatches(LoginRequest loginRequest) {
-        return false;
+    //--------------------------Password-Match--------------------------------
+    public boolean passwordMatch(String password, String encodedPassword) {
+        boolean passwordsMatch = this.passwordEncoder.matches(password, encodedPassword);
+        return passwordsMatch;
     }
-    //-------------------------------Login------------------------------------
-    public boolean login(LoginRequest loginRequest) {
-        return false;
+    //-----------------------------Get-User-----------------------------------
+    public User getUser(String username) {
+        return this.userRepository.getUserByUsername(username);
     }
-    //-------------------------------Signup-----------------------------------
-    public boolean signup(LoginRequest loginRequest) {
-        return false;
-    }
-    //=============================-Getters-==================================
+    //==============================-Getters-=================================
     public UserRepository getUserRepository() {
         return this.userRepository;
     }
