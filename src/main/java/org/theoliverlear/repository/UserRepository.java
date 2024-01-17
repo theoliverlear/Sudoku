97035ae7d1bf08636b4f7da7ff82ebf1e612b5c5
theoliverlear <sigwartholiver2@gmail.com>
2024-01-16 20:45:19 -0600

    Provide explicit SQL commands for JPA

--- src/main/java/org/theoliverlear/repository/UserRepository.java
+++ src/main/java/org/theoliverlear/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package org.theoliverlear.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.theoliverlear.entity.Board;
 import org.theoliverlear.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User getUserByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    User getUserByUsername(@Param("username") String username);
     User getUserById(Long id);
-    Long getBoardIdByUsername(String username);
+    @Query("SELECT u.currentBoard.id FROM User u WHERE u.username = ?1")
+    Long getBoardIdByUsername(@Param("username") String username);
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.username = ?1 AND u.currentBoard IS NOT NULL")
+    boolean boardExistsByUsername(@Param("username") String username);
 }
