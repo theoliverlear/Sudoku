f6820e0831d8904dc5f216c61a7912af13de3682
theoliverlear <sigwartholiver2@gmail.com>
2024-01-18 07:55:50 -0600

    Add a query for getting a time elapsed by a board id

--- src/main/java/org/theoliverlear/repository/BoardRepository.java
+++ src/main/java/org/theoliverlear/repository/BoardRepository.java
@@ -1,9 +1,12 @@
 package org.theoliverlear.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.theoliverlear.entity.Board;
-import org.theoliverlear.entity.User;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Board getBoardById(Long boardId);
+    @Query("SELECT b.timer FROM Board b WHERE b.id = ?1")
+    String getTimeById(@Param("id") Long boardId);
 }
